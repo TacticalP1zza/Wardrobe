61ceca9bef0ee91bc042f656a25b3e44fc798d4e
TacticalP1zza <thomas2004higginson@hotmail.com>
2024-05-31 17:10:37 +0100

    Hopefully opens a inv lets a player click a armor piece and equip it through /wardrobe, doesnt allow storing of any armor by the player and pretty sure will destory currently equipped armor. To do switch em

--- src/main/java/me/plainoldmoose/wardrobe/Wardrobe.java
+++ src/main/java/me/plainoldmoose/wardrobe/Wardrobe.java
@@ -1,17 +1,75 @@
 package me.plainoldmoose.wardrobe;
 
+package com.yourname.wardrobe;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Wardrobe extends JavaPlugin {
+public class WardrobePlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().info("Wardrobe Plugin Enabled");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Wardrobe Plugin Disabled");
+    }
+
+    // Command to open the wardrobe
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("wardrobe")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                openWardrobe(player);
+                return true;
+            } else {
+                sender.sendMessage("This command can only be used by players, are you an alien????");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void openWardrobe(Player player) {
+        Inventory wardrobe = Bukkit.createInventory(null, 27, "Wardrobe");
+
+        // Example: Add a set of armor
+        wardrobe.setItem(0, new ItemStack(Material.DIAMOND_HELMET));
+        wardrobe.setItem(1, new ItemStack(Material.DIAMOND_CHESTPLATE));
+        wardrobe.setItem(2, new ItemStack(Material.DIAMOND_LEGGINGS));
+        wardrobe.setItem(3, new ItemStack(Material.DIAMOND_BOOTS));
+        
+
+        player.openInventory(wardrobe);
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getView().getTitle().equals("Wardrobe")) {
+            event.setCancelled(true);  // Prevent taking items out of the wardrobe
+            Player player = (Player) event.getWhoClicked();
+            ItemStack clickedItem = event.getCurrentItem();
+            if (clickedItem != null && clickedItem.getType() != Material.AIR) {
+                // Equip the player with the clicked item
+                switch (event.getSlot()) {
+                    case 0: player.getInventory().setHelmet(clickedItem); break;
+                    case 1: player.getInventory().setChestplate(clickedItem); break;
+                    case 2: player.getInventory().setLeggings(clickedItem); break;
+                    case 3: player.getInventory().setBoots(clickedItem); break;
+                }
+            }
+        }
     }
 }
+
